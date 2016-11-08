04b088d3c86d1f74ed597e4159fb635df7febe84
bouti_n <bouti_n@fcd6bf73-a501-4a16-b634-a34a094c68c1>
2016-11-08 14:48:46 +0000

    etape 5

--- my_FTL/bouti_n/air_shed.c
+++ my_FTL/bouti_n/air_shed.c
@@ -14,7 +14,8 @@ t_ship	           *create_ship()
     }
   addr_ship = &ship;
   addr_ship->hull = 50;
-  addr_ship->weapons = NULL; 
+  addr_ship->weapons = NULL;
+  addr_ship->ftl_drive = NULL;
   my_putstr("amelioration du vaisseau termine!\n");
   return (addr_ship);
 }
@@ -25,14 +26,33 @@ int                add_weapon_to_ship(t_ship *addr_ship)
   t_weapon         weapon;
 
   my_putstr("ajout des armes en cours...\n");
-  weapon.damage = 10;
-  weapon.system_state = my_strdup("online");
-  addr_ship->weapons = &weapon;
+  addr_ship->weapons = malloc(sizeof(t_weapon*));
   if (!addr_ship->weapons)
   {
     my_putstr("votre vaisseau a explose quand vous avez tente d'ajouter les armes\n");
     return (0);
   }
+  addr_ship->weapons = &weapon;
+  addr_ship->weapons->damage = 10;
+  addr_ship->weapons->system_state = my_strdup("online");
   my_putstr("les armes ont ete ajoutees avec succes\n");
   return (1);
+}
+
+int                add_ftl_drive_to_ship(t_ship *addr_ship)
+{
+  t_ftl_drive      ftl_drive;
+
+  my_putstr("ajout du reacteur en cours...\n");
+  addr_ship->ftl_drive = malloc(sizeof(t_ftl_drive*));
+  if (!addr_ship->ftl_drive)
+  {
+    my_putstr("votre vaisseau a explose lorsque vous avez pose le reacteur !\n");
+    return(0);
+  }
+  addr_ship->ftl_drive = &ftl_drive;
+  addr_ship->ftl_drive->energy = 10;
+  addr_ship->ftl_drive->system_state = my_strdup("online");
+  my_putstr("Reacteur ajoute avec succes!\n");
+  return(1);
 }
--- my_FTL/bouti_n/main.c
+++ my_FTL/bouti_n/main.c
@@ -5,9 +5,11 @@ int main()
   t_ship *p_ship;
 
   p_ship = create_ship();
-  p_ship->hull = 1245;
   add_weapon_to_ship(p_ship);
   my_putstr(p_ship->weapons->system_state);
+  my_putstr("\n");
+  add_ftl_drive_to_ship(p_ship);
+  my_putstr(p_ship->ftl_drive->system_state);
   my_putstr("\n");
   return(0);
 }
