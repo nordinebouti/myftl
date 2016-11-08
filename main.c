cc71e1ca83e4e63e58f6b07fdc387210364ba3c4
bouti_n <bouti_n@fcd6bf73-a501-4a16-b634-a34a094c68c1>
2016-11-08 18:41:13 +0000

    etape 8

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 int main()
 {
   t_ship *p_ship;
-
   p_ship = create_ship();
   add_weapon_to_ship(p_ship);
   my_putstr(p_ship->weapons->system_state);
   my_putstr("\n");
   add_ftl_drive_to_ship(p_ship);
   my_putstr(p_ship->ftl_drive->system_state);
+  my_putstr("\n");
+  add_navigation_tools_to_ship(p_ship);
+  my_putstr(p_ship->nav_tools->system_state);
   my_putstr("\n");
   return(0);
 }
