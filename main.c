d2759767ffacbe1a22d43af43b64570d9a960f5a
bouti_n <bouti_n@fcd6bf73-a501-4a16-b634-a34a094c68c1>
2016-11-09 12:51:23 +0000

    etape 11

--- main.c
+++ main.c
@@ -8,9 +8,10 @@ int main()
   t_freight		*p_fr3;
   t_freight 	*test;
 
-	p_fr1 = malloc(sizeof(t_freight));
-	p_fr2 = malloc(sizeof(t_freight));  
-	p_fr3 = malloc(sizeof(t_freight));
+  test = NULL;
+  p_fr1 = malloc(sizeof(t_freight));
+  p_fr2 = malloc(sizeof(t_freight));  
+  p_fr3 = malloc(sizeof(t_freight));
   p_ship = create_ship();
   add_weapon_to_ship(p_ship);
   add_ftl_drive_to_ship(p_ship);
@@ -22,15 +23,17 @@ int main()
   add_freight_to_container(p_ship, p_fr1);
   add_freight_to_container(p_ship, p_fr2);
   add_freight_to_container(p_ship, p_fr3);
-  test = p_ship->cont->first;
+  del_from_to_container(p_ship, p_fr3);
+  test = p_ship->cont->last;
   while (test != NULL)
   {
   	my_putstr(test->item);
   	my_putstr("\n");
-  	test = test->next;
+  	test = test->prev;
   }
+  
   free(p_fr1);
   free(p_fr2);
-  free(p_fr3);
+  free(p_ship);
   return(0);
 }
